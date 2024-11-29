e7064fbb79b9888ef9bd30b7e0b36d153749a164
Suyog-B20-Git <suyog7375@gmail.com>
2024-11-29 12:19:09 +0530

    utils

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ColorsForApp {
+  ColorsForApp._();
+  // Theme Color
+  static const Color primaryColor = Color(0xffda2128);
+  static const Color primaryLightColor = Color(0xffff646c);
+  static const Color primaryExtraLightColor = Color(0xffffbdc0);
+  static const Color primaryExtraLightShadeColor = Color(0xffFFE5E5);
+  static const Color secondaryColor = Color(0xff3CC585);
+  static const Color secondaryLightColor = Color(0xff6acc9d);
+  static const Color secondaryExtraLightColor = Color(0xffa2d9be);
+  static const Color tertiaryColor = Color(0xFFABABAB);
+  static const Color tertiaryLightColor = Color(0xFFC7C7C7);
+  static const Color tertiaryExtraLightColor = Color(0xFFEFEFEF);
+
+  // App color
+  static const Color shadowColor = Color(0xffd5d5d5);
+  static const Color subTitleColor = Color(0xff9f9f9f);
+  static const Color yellow = Colors.yellow;
+
+  // todo name should proper as written above
+  static const Color colorBlackShade = Color(0xff2a2a2a);
+  static const Color colorYellowShade = Colors.amber;
+}
